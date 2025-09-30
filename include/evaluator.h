7df7eb734a534b8e14176100c86d80680d14cb89
Aman Kumar <aman.26262@gmail.com>
2025-09-30 23:47:43 +0530

    git messup

--- include/evaluator.h
+++ include/evaluator.h
@@ -3,13 +3,12 @@
 #include <functional>
 
 class Evaluator {
-  public: 
-    void initialize(const FilterCondition& condition) {
-        evaluator_ = LanguageParser::parse(condition);
-    }
-    bool evaluate(const std::vector<Key>& keys) {
-        return evaluator_(keys);
-    }
-  private:
-    std::function<bool(const std::vector<Key>&)> evaluator_;  
+public:
+  void initialize(const FilterCondition &condition) {
+    evaluator_ = LanguageParser::parse(condition);
+  }
+  bool evaluate(const std::vector<Key> &keys) { return evaluator_(keys); }
+
+private:
+  std::function<bool(const std::vector<Key> &)> evaluator_;
 };
--- include/key.h
+++ include/key.h
@@ -1,19 +1,20 @@
 #pragma once
 #include <string>
-#include <vector>
 #include <variant>
+#include <vector>
 
 using ValueType = std::variant<int64_t, double, std::string, bool>;
 
 class Key {
 public:
-    Key(const std::string& name, const ValueType& value) 
-        : name_(name), value_(value) {}
+  Key(const std::string &name, const ValueType &value)
+      : name_(name), value_(value) {}
 
-    const std::string& getName() const { return name_; }
-    const ValueType& getValue() const { return value_; }
-    void setValue(const ValueType& value) { value_ = value; }
+  const std::string &getName() const { return name_; }
+  const ValueType &getValue() const { return value_; }
+  void setValue(const ValueType &value) { value_ = value; }
+
 private:
-    std::string name_;
-    ValueType value_;
+  std::string name_;
+  ValueType value_;
 };
